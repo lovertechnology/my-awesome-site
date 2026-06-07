88c30f78f586b34fef36e27642a454a857c17be4
blucez <jiu.mie@outlook.com>
2026-06-07 18:00:46 +0800

    fix: 修复 tsconfig 类型定义与插件引用

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react' // ? 检查这里！改回你原本的 plugin-react，报错立刻消失
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
+  // ?? 动态路径雷达
+  base: process.env.GITHUB_ACTIONS === 'true' ? '/my-awesome-site/' : '/',
 })
